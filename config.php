4ca595ae437c75628c1bd1c10119da1b5e1b1740
tera-dev <terela.evgenij@gmail.com>
2020-02-03 14:09:30 +0200

    fix commit 5

--- config.php
+++ config.php
@@ -8,6 +8,6 @@ define('_CONTROLLERS_DIR_','./controllers/');
 define('_VIEWS_DIR_','./views/');
 define('_MODELS_DIR_','./models/');
 define('_LAYOUTS_DIR_','./layouts/');
-define('DB_DSN', 'mysql:host=testphp;dbname=test_php_db');
+define('DB_DSN', 'mysql:host=testphp;dbname=test_php_db;charset=utf8');
 define('DB_USER', 'root');
 define('DB_PASS', '');
